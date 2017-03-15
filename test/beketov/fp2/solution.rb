dff3198d646359ace06eea0eb0f43d3c17512170
Pavel Beketov <beketov@restream.rt.ru>
2017-03-16 01:54:05 +0400

    add fp2 tasks solutions

--- test/beketov/fp2/solution.rb
+++ test/beketov/fp2/solution.rb
@@ -1,4 +1,5 @@
-module Template
+# coding: utf-8
+module Beketov
   module Fp2
     class MyArray < Array
       # Использовать стандартные функции массива для решения задач нельзя.
@@ -6,18 +7,39 @@ module Template
 
       # Написать свою функцию my_each
       def my_each
+        for element in self
+          yield element
+        end
       end
 
       # Написать свою функцию my_map
       def my_map
+        result = MyArray.new([])
+        my_each do |element|
+          result << yield(element)
+        end
+        result
       end
 
       # Написать свою функцию my_compact
       def my_compact
+        my_reduce(MyArray.new([])) do |acc, element|
+          acc << element unless element.nil?
+          acc
+        end
       end
 
       # Написать свою функцию my_reduce
-      def my_reduce
+      def my_reduce(acc = nil)
+        if acc.nil?
+          acc = self[0]
+          skip = true
+        end
+        my_each do |element|
+          acc = yield(acc, element) unless skip
+          skip = false
+        end
+        acc
       end
     end
   end
