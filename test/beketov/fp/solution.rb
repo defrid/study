32d1dae779d9234fd3c3fa63ea7b5830f3084684
Pavel Beketov <beketov@restream.rt.ru>
2017-03-16 00:14:23 +0400

    add solutions for fp tasks and fix test file

--- test/beketov/fp/solution.rb
+++ test/beketov/fp/solution.rb
@@ -1,15 +1,25 @@
-module Template
+# coding: utf-8
+module Beketov
   module Fp
     class << self
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        needed_films = array.map do |film|
+          kp_rating = film['rating_kinopoisk'].to_f
+          kp_rating if !film['country'].nil? && film['country'].include?(',') &&
+                       !kp_rating.zero?
+        end .compact
+
+        needed_films.reduce(:+) / needed_films.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films
+          .select { |film| film['rating_kinopoisk'].to_f >= threshold }
+          .map { |film| film['name'].scan(/и/i).length }
+          .reduce(:+)
       end
     end
   end
