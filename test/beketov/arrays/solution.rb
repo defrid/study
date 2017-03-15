1babe23e58048b500cf8e6fbd7ec691aa5529ff5
Pavel Beketov <beketov@restream.rt.ru>
2017-03-16 00:28:57 +0400

    add recursive solution for binary search

--- test/beketov/arrays/solution.rb
+++ test/beketov/arrays/solution.rb
@@ -6,7 +6,15 @@ module Beketov
         array.map { |el| el > 0 ? max : el }
       end
 
-      def search(array, query)
+      def search_rec(array, query, left, right)
+        return -1 if left >= right
+        half = (left + right) / 2
+        return half if array[half] == query
+        return search_rec(array, query, left, half) if array[half] > query
+        search_rec(array, query, half + 1, right)
+      end
+
+      def search_cycle(array, query)
         right = array.length
         left = 0
         while left < right
@@ -21,6 +29,10 @@ module Beketov
         end
 
         -1
+      end
+
+      def search(array, query)
+        search_rec(array, query, 0, array.length)
       end
     end
   end
